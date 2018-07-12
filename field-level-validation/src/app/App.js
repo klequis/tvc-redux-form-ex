e94e9ed6f2f0161e3306f615187fafe1efc59972
klequis <klequis@gmail.com>
2018-07-12 07:48:26 -0700

    removed ;

--- field-level-validation/src/app/App.js
+++ field-level-validation/src/app/App.js
@@ -2,23 +2,7 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import ShowValues from './ShowValues'
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-)
 
-const required = value => value ? undefined : 'Required'
-const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
-const minValue = min => value =>
-  value && value < min ? `Must be at least ${min}` : undefined
-const minValue18 = minValue(18)
-const tooOld = value =>
-  value && value > 65 ? 'You might be too old for this' : undefined
 
 class FieldLevelValidation extends React.Component {
   state = {
@@ -35,17 +19,7 @@ class FieldLevelValidation extends React.Component {
     const { handleSubmit, pristine, reset, submitting } = this.props
     return (
       <div style={styles.wrapper}>
-        <form onSubmit={handleSubmit}>
-          <Field name="age" type="number"
-            component={renderField} label="Age"
-            validate={[ required, number, minValue18 ]}
-            warn={tooOld}
-          />
-          <div>
-            <button type="submit" disabled={submitting}>Submit</button>
-            <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
-          </div>
-        </form>               
+
         <ShowValues values={this.state.values}/>
       </div>
     )
--- simplest-form/src/app/App.js
+++ simplest-form/src/app/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
 import ShowValues from './ShowValues'
 
 
@@ -8,46 +7,18 @@ class SimpleForm extends React.Component {
     values: 'Fill in form and click Submit to see values'
   }
 
-  onSubmit = (values) => {
-    console.log('onSubmit: values', values)
-    this.setState({
-      values: values
-    })
-  }
+
   render() {
-    const { handleSubmit, pristine, reset, submitting } = this.props
     return (
       <div style={styles.wrapper}>
-        <form onSubmit={handleSubmit(this.onSubmit)}>
-          <div>
-            <label>First Name</label>
-            <div>
-              <Field
-                name="firstName"
-                component="input"
-                type="text"
-                placeholder="First Name"
-              />
-            </div>
-          </div>
-          
-          <div>
-            <button type="submit" disabled={pristine || submitting}>Submit</button>
-            <button type="button" disabled={pristine || submitting} onClick={reset}>
-              Clear Values
-            </button>
-          </div>
-          
-        </form>
+
         <ShowValues values={this.state.values}/>
       </div>
     );
   }
 };
 
-export default reduxForm({
-  form: 'simple'
-})(SimpleForm)
+
 
 const styles = {
   wrapper: {
